73ea9a9fd1418ca11d1f46e44d9528d2cdf7e0d4
zizzu94 <azamzihadul@hotmail.it>
2021-01-01 17:50:21 +0100

    Added output texts and code optimization

--- Vehicle_Detection.py
+++ Vehicle_Detection.py
@@ -1,10 +1,9 @@
 # ------------------------- created by: Zihadul Azam -------------------
 
 # import libraries
-import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 import vehicles # class file
+import utils
 import time
 
 # /#/#/#/#/#/#/#/#/#/#/#/# <> Global Variables /#/#/#/#/#/#/#/#/#/#/#/#
@@ -13,50 +12,20 @@ input_video_path = 'samples/sample_3.mp4'
 cap = cv2.VideoCapture(input_video_path)
 
 ratio = .45
-cnt_up = 0
-cnt_down = 0
-
-# colors
-heavy_vehicle_color = (0, 165, 255)
-normal_vehicle_color = (0, 255, 0)
 
 # Get width and height of video
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) * ratio
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) * ratio
 
 frameArea = height * width
-areaTH = frameArea / 400 # tracking height
+areaTH = frameArea / 400 # tracking area
 type_of_vehicle_area_treshold = 10000
 
-# Lines (four tracking lines)
-up_limit = int(6 * (height / 10))
-line_up = int(7 * (height / 10))
-line_down = int(8 * (height / 10))
-down_limit = int(9 * (height / 10))
-
-line_down_color = (255, 0, 0)
-line_up_color = (255, 0, 255)
-
-# points of down line
-pt1 = [0, line_down]
-pt2 = [width, line_down]
-pts_L1 = np.array([pt1, pt2], np.int32)
-pts_L1 = pts_L1.reshape((-1, 1, 2))
-# points of up line
-pt3 = [0, line_up]
-pt4 = [width, line_up]
-pts_L2 = np.array([pt3, pt4], np.int32)
-pts_L2 = pts_L2.reshape((-1, 1, 2))
-# points of up limit line
-pt5 = [0, up_limit]
-pt6 = [width, up_limit]
-pts_L3 = np.array([pt5, pt6], np.int32)
-pts_L3 = pts_L3.reshape((-1, 1, 2))
-# points of down limit line
-pt7 = [0, down_limit]
-pt8 = [width, down_limit]
-pts_L4 = np.array([pt7, pt8], np.int32)
-pts_L4 = pts_L4.reshape((-1, 1, 2))
+# Lines (detection lines and limit lines)
+up_limit = int(5 * (height / 10))
+line_up = int(6 * (height / 10))
+line_down = int(7 * (height / 10))
+down_limit = int(8 * (height / 10))
 
 # Background Subtractor
 fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
@@ -64,15 +33,19 @@ fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 # Kernal
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
 
-font = cv2.FONT_HERSHEY_DUPLEX
 my_vehicles = []
-max_p_age = 5
-pid = 1
+count_up = 0
+count_down = 0
+count_heavy = 0
+count_normal = 0
+max_vehicle_age = 5
+vehicle_id = 1
 
 # /#/#/#/#/#/#/#/#/#/#/#/# </> End Global Variables /#/#/#/#/#/#/#/#/#/#/#/#
 
 
 # /#/#/#/#/#/#/#/#/#/#/#/# <> Start Frame Analysis /#/#/#/#/#/#/#/#/#/#/#/#
+
 while(cap.isOpened()):
     ret, frame = cap.read()
 
@@ -81,8 +54,8 @@ while(cap.isOpened()):
         print("Can't show the video. Exiting ...")
         break
 
-    for i in my_vehicles:
-        i.age_one()
+    for vehicle in my_vehicles:
+        vehicle.age_one()
 
     # resize image (too big)
     image = cv2.resize(frame, (0, 0), None, ratio, ratio)
@@ -94,10 +67,10 @@ while(cap.isOpened()):
     fgmask = fgbg.apply(image)
 
     # Binarization
-    ret, imBin = cv2.threshold(fgmask, 200, 255, cv2.THRESH_BINARY)
+    ret, binary_img = cv2.threshold(fgmask, 200, 255, cv2.THRESH_BINARY)
 
     # OPening i.e First Erode the dilate
-    o_mask = cv2.morphologyEx(imBin, cv2.MORPH_OPEN, kernel)
+    o_mask = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel)
 
     # Closing i.e First Dilate then Erode
     c_mask = cv2.morphologyEx(o_mask, cv2.MORPH_CLOSE, kernel)
@@ -109,101 +82,83 @@ while(cap.isOpened()):
     #_,all_contours, hierarchy = cv2.findContours(c_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     _, all_contours, hierarchy = cv2.findContours(c_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    #---------------------------
-    normal_car = True
-    for cnt in all_contours:
-        area = cv2.contourArea(cnt)
+    normal_vehicle = True
+    for contour in all_contours:
+        area = cv2.contourArea(contour)
         if area > areaTH:
             ####Tracking######
-            m = cv2.moments(cnt)
-            cx = int(m['m10'] / m['m00'])
-            cy = int(m['m01'] / m['m00'])
-            x, y, w, h = cv2.boundingRect(cnt)
+            m = cv2.moments(contour)
+            cx = int(m['m10'] / m['m00']) # X of centroid
+            cy = int(m['m01'] / m['m00']) # Y of centroid
+
+            bounding_rect_x, bounding_rect_y, bounding_rect_w, bounding_rect_h = cv2.boundingRect(contour)
 
             new = True
+            # if the contour is inside the detection (count) zone
             if cy in range(up_limit, down_limit):
-                for i in my_vehicles:
-                    if abs(x - i.getX()) <= w and abs(y - i.getY()) <= h:
+                # check if current vehicle is already into the list
+                for vehicle in my_vehicles:
+                    if abs(bounding_rect_x - vehicle.getX()) <= bounding_rect_w and abs(bounding_rect_y - vehicle.getY()) <= bounding_rect_h:
                         new = False
-                        i.updateCoords(cx, cy)
+                        vehicle.updateCoords(cx, cy)
+                        vehicle.updateArea(area)
+                        vehicle.updateType(type_of_vehicle_area_treshold)
 
-                        if i.going_UP(line_down, line_up) == True:
-                            cnt_up += 1
-                            print("ID:", i.getId(),
-                                    'crossed going up at', time.strftime("%c"))
-                        elif i.going_DOWN(line_down, line_up) == True:
-                            cnt_down += 1
-                            print("ID:", i.getId(),
-                                    'crossed going up at', time.strftime("%c"))
+                        if vehicle.going_UP(line_up) == True:
+                            count_up += 1
+                            count_heavy += 1 if vehicle.type == 'heavy' else 0 
+                            count_normal += 1 if vehicle.type == 'normal' else 0 
+                            print("ID:", vehicle.getId(), 'crossed going [UP] at', time.strftime("%c"))
+                        elif vehicle.going_DOWN(line_down) == True:
+                            count_down += 1
+                            count_heavy += 1 if vehicle.type == 'heavy' else 0 
+                            count_normal += 1 if vehicle.type == 'normal' else 0 
+                            print("ID:", vehicle.getId(), 'crossed going [DOWN] at', time.strftime("%c"))
                         break
-                    if i.getState() == '1':
-                        if i.getDir() == 'down' and i.getY() > down_limit:
-                            i.setDone()
-                        elif i.getDir() == 'up' and i.getY() < up_limit:
-                            i.setDone()
-                    if i.timedOut():
-                        index = my_vehicles.index(i)
-                        my_vehicles.pop(index)
-                        del i
-
-                if new == True:  # If nothing is detected,create new
-                    p = vehicles.Car(pid, cx, cy, max_p_age)
-                    my_vehicles.append(p)
-                    pid += 1
+                    # set done if current vehicle already passed the up or down detection line
+                    if vehicle.getState() == '1':
+                        if vehicle.getDir() == 'down' and vehicle.getY() > down_limit:
+                            vehicle.setDone()
+                        elif vehicle.getDir() == 'up' and vehicle.getY() < up_limit:
+                            vehicle.setDone()
+                # create new
+                if new == True:  
+                    new_Vehicle = vehicles.Car(vehicle_id, cx, cy, area, max_vehicle_age)
+                    my_vehicles.append(new_Vehicle)
+                    vehicle_id += 1
 
             # classify vehicle by area: <normal, heavy>
             if area > type_of_vehicle_area_treshold:
-                normal_car = False
-            rect_color = normal_vehicle_color if normal_car else heavy_vehicle_color
+                normal_vehicle = False
 
-            # draw rectangle
-            cv2.circle(image, (cx, cy), 5, (0, 0, 255), -1)
-            img = cv2.rectangle(image, (x, y), (x + w, y + h), rect_color, 2)
+            # draw vehicle boundary rectangle and center point
+            utils.drawVehicleCenterPoint(image, cx, cy)
+            utils.drawVehicleBoundaryRect(image, bounding_rect_x, bounding_rect_y, bounding_rect_w, bounding_rect_h, normal_vehicle)
+
+    # Print vehicle Id if not timedOut else delete
+    for vehicle in my_vehicles:
+        if not vehicle.timedOut():
+            utils.printVehicleId(image, vehicle)
+        else:
+            index = my_vehicles.index(vehicle)
+            my_vehicles.pop(index)
+            del vehicle
             
-            # draw rectangle lable
-            label = 'Normal' if normal_car else 'Heavy'
-            labelSize, baseLine = cv2.getTextSize(label, font, 0.5, 1)
-            cv2.rectangle(image, (x - 1, y - labelSize[1] - 8), (x + labelSize[0] + 2, y), rect_color, cv2.FILLED)
-            cv2.putText(image, label, (x, y-6), font, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
 
-    # print car Id
-    for i in my_vehicles:
-        cv2.putText(image, str(i.getId()), (i.getX(), i.getY()),
-                    font, 0.3, i.getRGB(), 1, cv2.LINE_AA)
+    # Print detection lines
+    utils.printDetectionLines(image, line_up, line_down, up_limit, down_limit, width)
 
-    # print texts and lines
-    str_up = 'UP: ' + str(cnt_up)
-    str_down = 'DOWN: ' + str(cnt_down)
-    image = cv2.polylines(image, [pts_L1], False, line_down_color, thickness=2)
-    image = cv2.polylines(image, [pts_L2], False, line_up_color, thickness=2)
-    image = cv2.polylines(image, [pts_L3], False, (255, 255, 255), thickness=1)
-    image = cv2.polylines(image, [pts_L4], False, (255, 255, 255), thickness=1)
-    cv2.putText(image, str_up, (10, 40), font, 0.5,
-                (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(image, str_up, (10, 40), font,
-                0.5, (0, 0, 255), 1, cv2.LINE_AA)
-    cv2.putText(image, str_down, (10, 90), font,
-                0.5, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.putText(image, str_down, (10, 90), font,
-                0.5, (255, 0, 0), 1, cv2.LINE_AA)
-
+    # Print output texts (Total counts)
+    utils.printOutputTexts(image, count_up, count_down, count_normal, count_heavy)
     
-    # ######################## Displays images and transformations #########################
-    cv2.imshow('Frame', image)
-    cv2.moveWindow("Frame", 0, 0)
-
-    cv2.imshow("Subtraction and Binarization (threshold)", imBin)
-    cv2.moveWindow("Subtraction and Binarization (threshold)", int(width), 0)
-
-    cv2.imshow("Closing Morphology", c_mask)
-    cv2.moveWindow("Closing Morphology", 0, int(height))
-
-    cv2.imshow("Opening Morphology", o_mask)
-    cv2.moveWindow("Opening Morphology", int(width), int(height))
+    # Displays Image and Transformations 
+    utils.displayImageAndTransformations(image, binary_img, c_mask, o_mask, height, width)
 
     # control video speed
-    if cv2.waitKey(10) & 0xff == ord('q'):
+    if cv2.waitKey(20) & 0xff == ord('q'):
         break
+
+# /#/#/#/#/#/#/#/#/#/#/#/# </> End Frame Analysis /#/#/#/#/#/#/#/#/#/#/#/#
 
 cap.release()
 cv2.destroyAllWindows()
--- vehicles.py
+++ vehicles.py
@@ -3,19 +3,21 @@ import time
 
 class Car:
     tracks=[]
-    def __init__(self,i,xi,yi,max_age):
-        self.i=i
-        self.x=xi
-        self.y=yi
-        self.tracks=[]
-        self.R=randint(0,255)
-        self.G=randint(0,255)
-        self.B=randint(0,255)
-        self.done=False
-        self.state='0'
-        self.age=0
-        self.max_age=max_age
-        self.dir=None
+    def __init__(self,i,xi,yi,area,max_age):
+        self.i = i
+        self.x = xi
+        self.y = yi
+        self.area = area
+        self.tracks = []
+        self.R = randint(0,255)
+        self.G = randint(0,255)
+        self.B = randint(0,255)
+        self.done = False
+        self.state ='0'
+        self.age = 0
+        self.max_age = max_age
+        self.dir = None
+        self.type = 'normal'
 
     def getRGB(self):  #For the RGB colour
         return (self.R,self.G,self.B)
@@ -37,6 +39,24 @@ class Car:
     def getY(self):  #for y coordinate
         return self.y
 
+    def getArea(self):
+        return self.area
+
+    def updateArea(self, area):
+        self.area = area
+
+    def getType(self):
+        return self.type
+
+    def setType(self, type):
+        self.type = type
+
+    def updateType(self, treshold):
+        if self.area > treshold:
+            self.type = 'heavy'
+        else:
+            self.type = 'normal'
+
     def updateCoords(self, xn, yn):
         self.age = 0
         self.tracks.append([self.x, self.y])
@@ -49,11 +69,11 @@ class Car:
     def timedOut(self):
         return self.done
 
-    def going_UP(self, mid_start, mid_end):
+    def going_UP(self, detection_line):
         if len(self.tracks)>=2:
             if self.state=='0':
-                if self.tracks[-1][1]<mid_end and self.tracks[-2][1]>=mid_end:
-                    state='1'
+                if self.tracks[-1][1]<detection_line and self.tracks[-2][1]>=detection_line:
+                    self.state='1'
                     self.dir='up'
                     return True
                 else:
@@ -63,11 +83,11 @@ class Car:
         else:
             return False
 
-    def going_DOWN(self,mid_start,mid_end):
+    def going_DOWN(self, detection_line):
         if len(self.tracks)>=2:
             if self.state=='0':
-                if self.tracks[-1][1]>mid_start and self.tracks[-2][1]<=mid_start:
-                    start='1'
+                if self.tracks[-1][1]>detection_line and self.tracks[-2][1]<=detection_line:
+                    self.state='1'
                     self.dir='down'
                     return True
                 else:
@@ -82,4 +102,7 @@ class Car:
         if self.age>self.max_age:
             self.done=True
         return  True
+
+    def getAge(self):
+        return self.age
 
